3bfbc23224c4743a9dee499d26ea85f35f8c6ccc
zhaoyujieseberena <zhaoyujie@mail.ustc.edu.cn>
2023-09-25 17:15:36 +0800

    v3

--- src/csv2json.py
+++ src/csv2json.py
@@ -3,8 +3,8 @@ import csv
 import json
 
 def run_csv():
-    csv_file = './res/papers.csv'  # 替换为您的CSV文件路径
-    json_file = './res/papers.json'  # 替换为您想要生成的JSON文件路径
+    csv_file = './res/papers.csv'  
+    json_file = './res/papers.json'  
 
     data = []
 
@@ -14,10 +14,10 @@ def run_csv():
         for row in csvreader:
             data.append(row)
 
-    # 3. 创建JSON对象
+
     json_data = json.dumps(data, ensure_ascii=False, indent=4)
 
-    # 4. 将JSON对象写入文件
+
     with open(json_file, 'w', encoding='utf-8') as jsonfile:
         jsonfile.write(json_data)
 
@@ -32,10 +32,10 @@ def run_csv():
         for row in csvreader:
             data.append(row)
 
-    # 3. 创建JSON对象
+
     json_data = json.dumps(data, ensure_ascii=False, indent=4)
 
-    # 4. 将JSON对象写入文件
+
     with open(json_file, 'w', encoding='utf-8') as jsonfile:
         jsonfile.write(json_data)
     csv_file = './res/datasets.csv'  
@@ -49,12 +49,15 @@ def run_csv():
         for row in csvreader:
             data.append(row)
 
-    # 3. 创建JSON对象
+
     json_data = json.dumps(data, ensure_ascii=False, indent=4)
 
-    # 4. 将JSON对象写入文件
+
     with open(json_file, 'w', encoding='utf-8') as jsonfile:
         jsonfile.write(json_data)
+
+
+
 
     csv_file = './res/tools.csv'  
     json_file = './res/tools.json'  
@@ -67,10 +70,10 @@ def run_csv():
         for row in csvreader:
             data.append(row)
 
-    # 3. 创建JSON对象
+
     json_data = json.dumps(data, ensure_ascii=False, indent=4)
 
-    # 4. 将JSON对象写入文件
+
     with open(json_file, 'w', encoding='utf-8') as jsonfile:
         jsonfile.write(json_data)
 
